a0e81638ae4cc331d23e0b6874d7378588bcbc0f
ASB2 <aboysboy@gmail.com>
2013-05-23 15:27:55 -0400

    Commit 2 (5-23-13)

--- ASB2/common/TechCraft/lib/TEXTURES.java
+++ ASB2/common/TechCraft/lib/TEXTURES.java
@@ -3,10 +3,11 @@ package TechCraft.lib;
 public class TEXTURES {
 
     public static final String BLANK = "/mods/TechCraft/textures/blocks/White.png";
+    
     public static final String CONDUIT_EXPORTING = "/mods/TechCraft/textures/blocks/ModelMagicConduitExportingv2.png";
     public static final String CONDUIT_IMPORTING = "/mods/TechCraft/textures/blocks/ModelMagicConduitExportingv2.png";
     public static final String CONDUIT_MOVING = "/mods/TechCraft/textures/blocks/ModelMagicConduitExportingv2.png";
     
-    public static final String BASIC_RUNE = "/mods/TechCraft/textures/blocks/gui/RuneGui.png";
+    public static final String BASIC_RUNE = "/mods/TechCraft/textures/gui/RuneGui.png";
 
 }
