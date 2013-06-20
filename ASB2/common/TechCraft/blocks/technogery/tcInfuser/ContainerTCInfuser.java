505c789dc9f497d696faa1089b75b784c9a2ac9a
ASB2 <aboysboy@gmail.com>
2013-06-20 01:17:47 -0400

    Light number fix

--- ASB2/common/TechCraft/blocks/technogery/tcInfuser/ContainerTCInfuser.java
+++ ASB2/common/TechCraft/blocks/technogery/tcInfuser/ContainerTCInfuser.java
@@ -12,7 +12,7 @@ public class ContainerTCInfuser extends Container {
         
         this.addSlotToContainer(new Slot(tileEntity, 0, 28, 64));
         
-        this.addSlotToContainer(new Slot(tileEntity, 1, 32 + 15, 14)); 
+        this.addSlotToContainer(new Slot(tileEntity, 1, 47 + 15, 14)); 
         this.addSlotToContainer(new Slot(tileEntity, 2, 65 + 15, 14));         
         this.addSlotToContainer(new Slot(tileEntity, 3, 83 + 15, 14));
         
@@ -22,7 +22,7 @@ public class ContainerTCInfuser extends Container {
         
         this.addSlotToContainer(new Slot(tileEntity, 7, 47 + 15, 50));
         this.addSlotToContainer(new Slot(tileEntity, 8, 65 + 15, 50));        
-        this.addSlotToContainer(new Slot(tileEntity, 9, 83 + 15,50));
+        this.addSlotToContainer(new Slot(tileEntity, 9, 83 + 15 ,50));
         
         for (int i = 0; i < 3; i++)
         {
